a801f1c4506aa0c1591508920bddcb325ea2ca07
emilybowersox <emilygbowersox@gmail.com>
2021-06-01 09:16:52 -0500

    knock-knock game

---
+++ Knock-Knock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Knock_Knock
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+        WhosThere:
+            Console.WriteLine("Knock knock!");
+            String resp = Console.ReadLine().ToLower();
+            if (resp.Contains("who's there"))
+            {
+                goto OwlsSay;
+            }
+            else
+            {
+                goto WhosThere;
+            }
+
+        OwlsSay:
+            Console.WriteLine("Owls say");
+            String nextResp = Console.ReadLine().ToLower();
+            if (nextResp.Contains("owls say who"))
+            {
+                Console.WriteLine("Yes. Yes, they do.");
+                goto WhosThere;
+            }
+            else
+            {
+                goto OwlsSay;
+            }
+
+
+        }
+    }
+}
